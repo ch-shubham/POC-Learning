763dacb2ccd48ed8f9105e6c19f2ca08e87381bf
Shubham Chaudhary <shubham128166@gmail.com>
2024-07-09 01:19:55 +0530

    Adds getBucket with minio integration

---
+++ min-io/minio/src/main/java/io/poc/minio/MinioConfig.java
@@ -0,0 +1,26 @@
+package io.poc.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.accessKey}")
+    String accessKey;
+
+    @Value("${minio.secretKey}")
+    String secretKey;
+
+    @Bean
+    MinioClient minioClient(){
+        System.out.println(accessKey);
+        System.out.println(secretKey);
+        return MinioClient.builder()
+                        .endpoint("http://172.18.0.2:9000")
+                        .credentials(accessKey, secretKey)
+                        .build();
+    }
+}
--- min-io/minio/src/main/java/io/poc/minio/resouces/MinIOController.java
+++ min-io/minio/src/main/java/io/poc/minio/resouces/MinIOController.java
@@ -1,16 +1,43 @@
 package io.poc.minio.resouces;
 
+import io.minio.MinioClient;
+import io.minio.errors.*;
+import io.minio.messages.Bucket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class MinIOController {
 
+    @Autowired
+    MinioClient minioClient;
+
     @GetMapping("/health")
     public ResponseEntity<String> getHealth(){
         return ResponseEntity.ok("UP");
+    }
+
+    @GetMapping("/getBucketList")
+    public ResponseEntity<Integer> getBucketList() {
+        try{
+            List<Bucket> bucketList = minioClient.listBuckets();
+            return ResponseEntity.ok(bucketList.size());
+
+        }
+        catch (Exception err){
+            System.out.println("------------------------");
+            System.out.println(err);
+            System.out.println("------------------------");
+            return ResponseEntity.internalServerError().body(-1);
+        }
     }
 }
