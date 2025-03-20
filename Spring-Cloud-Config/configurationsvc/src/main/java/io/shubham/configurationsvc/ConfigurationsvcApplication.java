ed30cd8945a1481ed8c839f68d7d633c3fde3c89
ch-shubham <shubham128166@gmail.com>
2025-03-21 00:31:05 +0530

    Adds Spring-Cloud-Config-Service completely working

--- Spring-Cloud-Config/configurationsvc/src/main/java/io/shubham/configurationsvc/ConfigurationsvcApplication.java
+++ Spring-Cloud-Config/configurationsvc/src/main/java/io/shubham/configurationsvc/ConfigurationsvcApplication.java
@@ -1,9 +1,13 @@
 package io.shubham.configurationsvc;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEncryptableProperties
 public class ConfigurationsvcApplication {
 
 	public static void main(String[] args) {
